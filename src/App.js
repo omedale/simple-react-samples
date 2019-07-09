0fb8d51f6e88f31af361730f90ea8ad4f03ab69a
omedale <omedale@gmail.com>
2019-07-09 13:17:44 +0100

    passing methods reference

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = pros => {
-  const [personsState, setPersonsState] = useState({
+
+class App extends Component {
+  state = {
     persons: [
       { name: 'Medale', age: 34 },
       { name: 'Femi', age: 16 },
       { name: 'Ayo', age: 20 }
     ]
-  })
+  }
 
-  const switchNameHandler = () => {
-    setPersonsState((prevSate, props) => {
+  switchNameHandler = (newName) => {
+    this.setState((prevSate, props) => {
       const personList = [...prevSate.persons]
-      personList[1].name = "Faith";
+      personList[1].name = newName;
       personList[1].age = 0
       return {
         persons: personList
@@ -22,34 +23,44 @@ const app = pros => {
     })
   }
 
+
+  render() {
     return (
       <div className="App">
       <h1> hello</h1>
-      <button onClick={switchNameHandler}>Switch name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>
+      <button onClick={() => this.switchNameHandler('Joy')}>Switch name</button>
+      <Person
+        name={this.state.persons[0].name}
+        age={this.state.persons[0].age} />
+      <Person
+        name={this.state.persons[1].name}
+        age={this.state.persons[1].age}
+        click={this.switchNameHandler.bind(this, 'Kunle')}  />
+      <Person
+        name={this.state.persons[2].name}
+        age={this.state.persons[2].age}>
         My hoobies: Driving
       </Person>
       </div>
     );
+  }
 }
 
-export default app;
+export default App;
 
 
-// Using state with class
-// class App extends Component {
-//   state = {
+// Using functional useState() hook
+// const app = pros => {
+//   const [personsState, setPersonsState] = useState({
 //     persons: [
 //       { name: 'Medale', age: 34 },
 //       { name: 'Femi', age: 16 },
 //       { name: 'Ayo', age: 20 }
 //     ]
-//   }
+//   })
 
-//   switchNameHandler = () => {
-//     this.setState((prevSate, props) => {
+//   const switchNameHandler = () => {
+//     setPersonsState((prevSate, props) => {
 //       const personList = [...prevSate.persons]
 //       personList[1].name = "Faith";
 //       personList[1].age = 0
@@ -59,21 +70,17 @@ export default app;
 //     })
 //   }
 
-
-//   render() {
 //     return (
 //       <div className="App">
 //       <h1> hello</h1>
-//       <button onClick={this.switchNameHandler}>Switch name</button>
-//       <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-//       <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
-//       <Person name={this.state.persons[2].name} age={this.state.persons[2].age}>
+//       <button onClick={switchNameHandler}>Switch name</button>
+//       <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
+//       <Person name={personsState.persons[1].name} age={personsState.persons[1].age} />
+//       <Person name={personsState.persons[2].name} age={personsState.persons[2].age}>
 //         My hoobies: Driving
 //       </Person>
 //       </div>
 //     );
-//   }
 // }
 
-// export default App;
-
+// export default app;
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
   return (
     <div>
-      <p>I am { props.name } and I am {props.age} years olds</p>
+      <p onClick={props.click}> I am { props.name } and I am {props.age} years olds</p>
       <p>{props.children}</p>
     </div>
     )
